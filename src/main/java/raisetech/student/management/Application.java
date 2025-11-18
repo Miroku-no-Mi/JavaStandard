3f78c7086e641105f419b9e750c5956942047408
Miroku-no-Mi <l2406840518as@gmail.com>
2025-11-18 19:12:01 +0900

    第10回課題

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -1,56 +1,30 @@
 package raisetech.student.management;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-    private String name = "Enami Kouji";
-    private String age = "37";
-
-    private Map<String, String> student = new HashMap<>(); // studentの初期化
+    @Autowired
+    private StudentRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @GetMapping("/studentInfo") // 登録済み Student
-    public Object getStudentInfo() {
-        if  (student.isEmpty()){
-            return "登録された学生情報はありません。";
-        }
-        return student;
+    @GetMapping("/studentList")
+    public List<Student> getStudentList() {
+        return repository.search();
     }
 
-    @PostMapping("/studentInfo") // 新規登録 Student
-    public String addStudentInfo(@RequestParam String name, @RequestParam String age) {
-
-        if (student == null) { //　実装の度、ゼロに戻る　"初期化"
-            student = new HashMap<>();
-        }
-
-        student.put(name, age); //名前と年齢がセットで登録され、表示する
-        return name + " " + age + "歳";
-    }
-
-    @PutMapping("/studentInfo") // データ更新
-    public String updatestudent(@RequestParam String oldName, @RequestParam String newName) {
-        if (student == null || !student.containsKey(oldName)) {
-            return oldName;
-        }
-
-        String age = student.remove(oldName);
-        student.put(newName, age);
-        return oldName + "→" + newName;
-
+    @GetMapping("/studentCourses")
+    public List<StudentCourse> getStudentCoursesList() {
+        return repository.searchStudentCourses();
     }
 }
-
-
-//修正
---
+++ src/main/java/raisetech/student/management/StudentRepository.java
@@ -0,0 +1,16 @@
+package raisetech.student.management;
+
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+public interface StudentRepository {
+
+    @Select("SELECT * FROM students")
+    List<Student> search();
+
+    @Select("SELECT * FROM students_courses")
+    List<StudentCourse> searchStudentCourses();
+}
+
