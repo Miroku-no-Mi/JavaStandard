8626071e0024ff98b0b057f2bd9c4f2dd2ae5c7c
Miroku-no-Mi <l2406840518as@gmail.com>
2026-01-17 10:16:42 +0900

    第16回課題

--- src/main/java/raisetech/student/management/repository/StudentRepository.java
+++ src/main/java/raisetech/student/management/repository/StudentRepository.java
@@ -5,36 +5,29 @@ import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentsCourses;
 import java.util.List;
 
-/**
- * 受講生情報を扱うリポジトリ。
- * 全件検索や単一条件での検索、コース情報の検索が行えるクラスです。
- */
-
 @Mapper
 public interface StudentRepository {
 
-    @Select("SELECT * FROM students WHERE id = #{id}")
-    Student searchStudentById(Integer id);
 
-    @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
-    List<StudentsCourses> searchStudentsCoursesByStudentId(Integer studentId);
-
-    //削除されていない受講生を全件検索します。
-
-    @Select("SELECT * FROM students")
+    @Select("SELECT * FROM students WHERE is_deleted = false")
     List<Student> search();
 
+    @Select("SELECT * FROM students WHERE id = #{id}")
+    Student searchStudent(String id);
+
     @Select("SELECT * FROM students_courses")
-    List<StudentsCourses> searchStudentsCourses();
+    List<StudentsCourses> searchStudentsCoursesList();
+
+    @Select("SELECT * FROM students_courses WHERE student_id = #{studentId}")
+    List<StudentsCourses> searchStudentsCourses(String studentId);
 
 
-    // 新規登録（INSERT)
-
-    @Insert("INSERT INTO students(name, kana, nickname, email, area, age, sex, remark, is_deleted)"
-        + "VALUES(#{name}, #{kana}, #{nickname}, #{email}, #{area}, #{age}, #{sex}, #{remark}, false)")
+    @Insert("INSERT INTO students(name, kana_name, nickname, email, area, age, sex, remark, is_deleted)"
+        + "VALUES(#{name}, #{kanaName}, #{nickname}, #{email}, #{area}, #{age}, #{sex}, #{remark}, false)")
 
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void registerStudent(Student student);
+
 
     @Insert("INSERT INTO students_courses( student_id, course_name, course_start_at, course_end_at) " +
             "VALUES(#{studentId}, #{courseName}, #{courseStartAt}, #{courseEndAt})")
@@ -42,19 +35,13 @@ public interface StudentRepository {
     @Options(useGeneratedKeys = true, keyProperty = "id")
     void registerStudentsCourses(StudentsCourses studentsCourses);
 
-    @Update("""
-        UPDATE students
-        SET
-        name=#{name},
-        kana=#{kana},
-        email=#{email},
-        area=#{area},
-        age=#{age},
-        sex=#{sex},
-        remark=#{remark}
-        WHERE id = #{id}
-        """)
+
+    @Update(" UPDATE students SET name = #{name}, kana_name = #{kanaName}, nickname = #{nickname}," +
+            "email = #{email}, area = #{area}, age = #{age}, sex = #{sex}, remark = #{remark}, is_deleted = #{isDeleted} WHERE id = #{id} ")
     void updateStudent(Student student);
+
+    @Update(" UPDATE students_courses SET course_name = #{courseName} WHERE id = #{id}")
+    void updateStudentsCourses(StudentsCourses studentsCourses);
 
 
 }
