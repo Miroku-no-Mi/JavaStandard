d9eb51181c7d1dd25c15f6f408742b544d07c103
Miroku-no-Mi <l2406840518as@gmail.com>
2026-01-31 22:58:55 +0900

    第20回課題

--- src/main/java/raisetech/student/management/service/StudentService.java
+++ src/main/java/raisetech/student/management/service/StudentService.java
@@ -3,44 +3,66 @@ package raisetech.student.management.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import raisetech.student.management.controller.converter.StudentConverter;
 import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentsCourses;
+import raisetech.student.management.domain.StudentSearchCondition;
 import raisetech.student.management.repository.StudentRepository;
 import raisetech.student.management.domain.StudentDetail;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+/**
+ * 受講生情報を取り扱うサービスです。
+ * 受講生の検索や登録・更新処理を行います。
+ */
+
 @Service
 public class StudentService {
 
     private StudentRepository repository;
+    private StudentConverter converter;
 
     @Autowired
-    public StudentService(StudentRepository repository) {
+    public StudentService(StudentRepository repository, StudentConverter converter) {
         this.repository = repository;
+        this.converter = converter;
     }
 
-    public List<Student> searchStudentList() {
-        return repository.search();
+    /**
+     * 受講生一覧検索です。
+     *      * 全件検索を行うので、条件指定は行いません。
+     * @return 受講生一覧（全件）
+     */
+    public List<StudentDetail> searchStudentList() {
+        List<Student> studentList = repository.search();
+        List<StudentsCourses> studentsCoursesList = repository.searchStudentsCoursesList();
+        return converter.convertStudentDetails(studentList, studentsCoursesList);
     }
+
+    public List<StudentDetail> searchStudents(StudentSearchCondition condition) {
+        List<Student> students = repository.searchByCondition(condition);
+        List<StudentsCourses> courses = repository.searchStudentsCoursesList();
+        return converter.convertStudentDetails(students, courses);
+    }
+
+    /**
+     * 受講生検索です。
+     * IDに紐づく受講生情報を取得したあと、その受講生に紐づく受講生コース情報を取得して設定します。
+     *
+     * @param id 受講生ID
+     * @return 受講生
+     */
 
     public StudentDetail searchStudent(String id) {
         Student student = repository.searchStudent(id);
         List<StudentsCourses> studentsCourses = repository.searchStudentsCourses(student.getId());
-        StudentDetail studentDetail = new StudentDetail();
-        studentDetail.setStudent(student);
-        studentDetail.setStudentsCourses(studentsCourses);
-        return studentDetail;
+        return new StudentDetail(student,studentsCourses);
     }
-
-    public List<StudentsCourses> searchStudentsCoursesList() {
-        return repository.searchStudentsCoursesList();
-    }
-
 
     @Transactional
-    public void registerStudent(StudentDetail studentDetail) {
+    public StudentDetail registerStudent(StudentDetail studentDetail) {
         repository.registerStudent(studentDetail.getStudent());
         for (StudentsCourses studentsCourse : studentDetail.getStudentsCourses()) {
             studentsCourse.setStudentId(studentDetail.getStudent().getId());
@@ -48,6 +70,7 @@ public class StudentService {
             studentsCourse.setCourseEndAt(LocalDateTime.now().plusYears(1));
             repository.registerStudentsCourses(studentsCourse);
         }
+        return studentDetail;
     }
     @Transactional
     public void updateStudent(StudentDetail studentDetail){
